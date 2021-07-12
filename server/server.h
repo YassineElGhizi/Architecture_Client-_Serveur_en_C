4d50e7a2939ca4c0db8b4c463ea2ba959bfd226c
YassineElGhizi <86019502+YassineElGhizi@users.noreply.github.com>
2021-07-12 11:36:26 +0100

    Update server.h

--- server/server.h
+++ server/server.h
@@ -1,6 +1,3 @@
-//Encadre Par : Pr M.Aiy kbir
-
-//Devloppe Par : EL GHIZI Yassine - Afkir Hamza
 #ifndef BACKEND_H_INCLUDED
     #define BACKEND_H_INCLUDED
     void aff_addr(struct sockaddr_in x);
