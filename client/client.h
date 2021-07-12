efcdd4788a887bd3e367941273f546fbb70b9aa7
YassineElGhizi <86019502+YassineElGhizi@users.noreply.github.com>
2021-07-12 11:33:47 +0100

    Update client.h

--- client/client.h
+++ client/client.h
@@ -1,7 +1,3 @@
-//Encadre Par : Pr M.Aiy kbir
-
-//Devloppe Par : EL GHIZI Yassine - Afkir Hamza
-
 #ifndef BACKENDC_H_INCLUDED
     #define BACKENDC_H_INCLUDED
     void aff_addr(struct sockaddr_in addr);
